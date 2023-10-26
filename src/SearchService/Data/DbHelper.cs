c080bb6893863e031ba6930a9500cc3815374ce4
qalshakhoori <qalshakhoori@moic.gov.bh>
2023-10-26 09:42:06 +0300

    Adding Search Service

---
+++ src/SearchService/Data/DbHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.Services;
+
+namespace SearchService;
+
+public class DbHelper
+{
+  public static async Task InitDb(WebApplication app)
+  {
+    await DB.InitAsync("SearchDB", MongoClientSettings
+      .FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+    await DB.Index<Item>()
+      .Key(x => x.Make, KeyType.Text)
+      .Key(x => x.Model, KeyType.Text)
+      .Key(x => x.Color, KeyType.Text)
+      .CreateAsync();
+
+    var count = await DB.CountAsync<Item>();
+
+    using var scope = app.Services.CreateScope();
+
+    var client = scope.ServiceProvider.GetRequiredService<AuctionServiceHttpClient>();
+
+    var items = await client.GetItemsForSearchDB();
+
+    Console.WriteLine(items.Count + " returned from the auction service");
+
+    if (items.Count > 0)
+      await DB.SaveAsync(items);
+  }
+}
---
+++ src/SearchService/Services/AuctionServiceHttpClient.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services;
+
+public class AuctionServiceHttpClient
+{
+  private readonly HttpClient _client;
+  private readonly IConfiguration _config;
+
+  public AuctionServiceHttpClient(HttpClient client, IConfiguration config)
+  {
+    _client = client;
+    _config = config;
+  }
+
+  public async Task<List<Item>> GetItemsForSearchDB()
+  {
+    var lastUpdated = await DB.Find<Item, string>()
+      .Sort(x => x.Descending(x => x.UpdatedAt))
+      .Project(x => x.UpdatedAt.ToString())
+      .ExecuteFirstAsync();
+
+    return await _client.GetFromJsonAsync<List<Item>>
+      (_config["AuctionServiceUrl"] + "/api/auctions?date=" + lastUpdated);
+  }
+}

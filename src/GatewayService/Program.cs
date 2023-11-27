435d2771b6a13812a28d18ba939bfd546e683419
qalshakhoori <qalshakhoori@moic.gov.bh>
2023-11-27 10:05:26 +0300

    notification service

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -14,7 +14,20 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
   options.TokenValidationParameters.NameClaimType = "username";
 });
 
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("customPolicy", b =>
+  { // b for build
+    b.AllowAnyHeader()
+      .AllowAnyMethod()
+      .AllowCredentials()
+      .WithOrigins(builder.Configuration["ClientApp"]);
+  });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,29 @@
+using MassTransit;
+using NotificationService;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(m =>
+{
+  m.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+  m.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+
+  m.UsingRabbitMq((context, config) =>
+  {
+    config.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+    {
+      host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+      host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+    });
+    config.ConfigureEndpoints(context);
+  });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<NotificationHub>("/notifications");
+
+app.Run();
